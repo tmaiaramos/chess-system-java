8d7d7a0a999476fd93a39105a456cf9d5c998426
Tiago Maia Ramos <tramos@mpmg.mp.br>
2026-05-12 15:34:41 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.util.Scanner;
+
+import chess.ChessPiece;
 import chess.ChessMatch;
+import chess.ChessPosition;
 
 /**
  * Ponto de entrada da aplicação do jogo de xadrez.
@@ -25,7 +29,21 @@ public class Program {
      * @param args argumentos de linha de comando (não utilizados)
      */
     public static void main(String[] args) {        
-        ChessMatch chessMatch = new ChessMatch();        
-        UI.printBoard(chessMatch.getPieces());
+        
+        Scanner sc = new Scanner(System.in);
+        ChessMatch chessMatch = new ChessMatch(); 
+
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+            
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
