1a2982c4f4a79090dafe45539789cbe44bc3d932
Tiago Maia Ramos <tramos@mpmg.mp.br>
2026-05-12 21:11:39 -0300

    Handling exceptions and clearing screen

--- compile.bat
+++ compile.bat
@@ -1,4 +1,13 @@
 @echo off
 mkdir bin 2>nul
-javac -d bin src\*.java src\chess\*.java src\chess\pieces\*.java
-echo Compilado com sucesso!
+echo Gerando lista de arquivos...
+dir /s /B src\*.java > sources.txt
+echo Compilando...
+javac -d bin @sources.txt
+del sources.txt
+if %errorlevel% equ 0 (
+    echo Compilado com sucesso!
+) else (
+    echo Erro na compilacao!
+)
+pause
--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import chess.ChessPiece;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPosition;
 
@@ -34,16 +36,26 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch(); 
 
         while (true) {
+            try {
+            UI.clearScreen();
             UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
 
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
-            
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
     }
 }
